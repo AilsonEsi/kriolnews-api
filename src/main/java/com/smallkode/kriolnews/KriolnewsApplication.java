83a90956ed02261efd24366fe033c7bbb2aeed29
Ailson Moreira <ailsonesi96@gmail.com>
2019-04-11 02:21:31 -0100

    Initial commit

---
+++ src/main/java/com/smallkode/kriolnews/KriolnewsApplication.java
@@ -0,0 +1,13 @@
+package com.smallkode.kriolnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KriolnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KriolnewsApplication.class, args);
+	}
+
+}
